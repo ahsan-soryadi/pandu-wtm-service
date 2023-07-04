211ba0bf38022ef0fb7753ddfadae068fcb96b6b
Ahsan-soryadi <assandis@gmail.com>
2023-07-05 00:27:02 +0700

    Changes to be committed: 	modified:   stockOpname.js 	modified:   stockOpnameDAO.js

--- stockOpname.js
+++ stockOpname.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { addStockOpname } = require('./stockOpnameDAO')
+const { addStockOpname, getAllStockOpname, getAllStockOpnameDetails } = require('./stockOpnameDAO')
 const router = express.Router()
 
 router.post('/addStockOpname', async (req, res) => {
@@ -9,12 +9,24 @@ router.post('/addStockOpname', async (req, res) => {
     const lokasiGudangId = req.body.lokasiGudangId
 
     const result = await addStockOpname(barang, usernameId, tanggalSo, lokasiGudangId)
-    console.log("hasil add stock opname ", result)
+    // console.log("hasil add stock opname ", result)
     if(result.affectedRows > 0){
         res.send({"message": "ok"})
     } else {
         res.send({"message": "error"})
     }
+})
+
+router.post('/getAllStockOpname', async (req, res) => {
+    const lokasiGudangId = req.body.lokasiGudangId
+    const result = await getAllStockOpname(lokasiGudangId)
+    res.send(result)
+})
+
+router.post('/getAllStockOpnameDetails', async (req, res) => {
+    const stockOpnameId = req.body.stockOpnameId
+    const result = await getAllStockOpnameDetails(stockOpnameId)
+    res.send(result)
 })
 
 module.exports = router
--- stockOpnameDAO.js
+++ stockOpnameDAO.js
@@ -5,14 +5,14 @@ exports.addStockOpname = async (barang = [], usernameId = '', tanggalSo = Date()
         return "barang kosong"
     }
     
-    console.log("barang from addStockOpname = ",barang)
+    // console.log("barang from addStockOpname = ",barang)
     const dbConn = await conn()
     const sqlString = 'INSERT INTO STOCK_OPNAME_BARU (usernameID, tanggal_so, lokasiGudangID) VALUES(?,?,?)'
-    const sqlString2 = 'INSERT INTO STOCK_OPNAME_MATCH_STATUS (serialNumber, matchStatus, stockOpnameBaruID) VALUES ?'
+    const sqlString2 = 'INSERT INTO STOCK_OPNAME_MATCH_STATUS (serialNumber, matchStatus, stockOpnameBaruID, jenisBarang) VALUES ?'
     try {
         const [insertResult] = await dbConn.query(sqlString, [usernameId, tanggalSo, lokasiGudangId])
         if(insertResult.insertId !== 0 ){
-            let barangTemp = barang.map(item => [item.serialNumber, item.match, insertResult.insertId])
+            let barangTemp = barang.map(item => [item.serialNumber, item.match, insertResult.insertId, item.jenisBarang])
            const [rows] = await dbConn.query(sqlString2, [barangTemp]) 
                 return rows
         } else {
@@ -24,4 +24,42 @@ exports.addStockOpname = async (barang = [], usernameId = '', tanggalSo = Date()
         await dbConn.end()
     }
    
+}
+
+exports.getAllStockOpname = async (lokasiGudangId = 0) =>{
+    if(lokasiGudangId === 0){
+        return "lokasi gudang kosong"
+    }
+    const dbConn = await conn()
+    const sqlString = `SELECT id, 
+                        (SELECT username FROM username WHERE username.id = stock_opname_baru.usernameID) 
+                        AS createdBy, 
+                        tanggal_so, 
+                        (SELECT nama_gudang from lokasi_gudang WHERE lokasi_gudang.id = stock_opname_baru.lokasiGudangID)
+                        AS lokasiGudang 
+                        FROM stock_opname_baru WHERE lokasiGudangID = ?`
+    try {
+        const [rows] = await dbConn.query(sqlString, lokasiGudangId)
+        return rows
+    } catch (error) {
+        console.log(error)
+    } finally {
+        await dbConn.end()
+    }
+}
+
+exports.getAllStockOpnameDetails = async (stockOpnameId = 0) => {
+    if(stockOpnameId === 0){
+        return []
+    }
+    const dbConn = await conn()
+    const sqlString = 'SELECT serialNumber, matchStatus, jenisBarang FROM STOCK_OPNAME_MATCH_STATUS WHERE stockOpnameBaruID = ?'
+    try {
+        const [rows] = await dbConn.query(sqlString, [stockOpnameId])
+        return rows
+    } catch (error) {
+        console.log(error)
+    } finally {
+        await dbConn.end()
+    }
 }
